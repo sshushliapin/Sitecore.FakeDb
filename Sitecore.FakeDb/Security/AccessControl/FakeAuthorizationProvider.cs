970832165696fbbc674652a63a43192def019fe6
Sergey Shushlyapin <ss@sitecore.net>
2014-01-03 14:47:58 +0200

    Configured authorization provider

---
+++ Sitecore.FakeDb/Security/AccessControl/FakeAuthorizationProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.FakeDb.Security.AccessControl
+{
+  using Sitecore.Security.AccessControl;
+  using Sitecore.Security.Accounts;
+
+  public class FakeAuthorizationProvider : AuthorizationProvider
+  {
+    public override AccessRuleCollection GetAccessRules(ISecurable entity)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public override void SetAccessRules(ISecurable entity, AccessRuleCollection rules)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    protected override AccessResult GetAccessCore(ISecurable entity, Account account, AccessRight accessRight)
+    {
+      return new AccessResult(AccessPermission.Allow, new AccessExplanation("Everything is allowed by design."));
+    }
+  }
+}
