d014b9f783773ea3e130968feb255012cffd4b4d
Sergey Shushlyapin <sshushlyapin@gmail.com>
2014-02-03 00:35:04 +0200

    Generate nuget package

--- Source/Sitecore.FakeDb.Tests/Properties/AssemblyInfo.cs
+++ Source/Sitecore.FakeDb.Tests/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.FakeDb.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A friendly unit testing framework for Sitecore.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sitecore")]
 [assembly: AssemblyProduct("Sitecore.FakeDb.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
--- Source/Sitecore.FakeDb/Properties/AssemblyInfo.cs
+++ Source/Sitecore.FakeDb/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.FakeDb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A friendly unit testing framework for Sitecore.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sitecore")]
 [assembly: AssemblyProduct("Sitecore.FakeDb")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
