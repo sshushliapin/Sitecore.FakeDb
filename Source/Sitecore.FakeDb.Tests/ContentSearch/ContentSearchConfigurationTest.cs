46bcefe7847ca504915162feb3779ae91a757656
Sergey Shushlyapin <ss@sitecore.net>
2014-02-06 16:57:17 +0200

    Added content search configuration. Can unit test search indexes

---
+++ Source/Sitecore.FakeDb.Tests/ContentSearch/ContentSearchConfigurationTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.FakeDb.Tests.ContentSearch
+{
+  using System;
+  using FluentAssertions;
+  using Sitecore.ContentSearch;
+  using Xunit;
+
+  public class ContentSearchConfigurationTest
+  {
+    [Fact]
+    public void ShouldGetContentSearchConfiguration()
+    {
+      // act
+      Action a = () => { var c = ContentSearchManager.SearchConfiguration; };
+
+      // assert
+      a.ShouldNotThrow();
+    }
+  }
+}
