30b7eb8978ace9c22f4767837d683b4c6986a299
Sergey Shushlyapin <ss@sitecore.net>
2014-02-06 17:22:10 +0200

    Added authentication provider stub

---
+++ Source/Sitecore.FakeDb.Tests/Security/Authentication/AuthenticationManagerTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.FakeDb.Tests.Security.Authentication
+{
+  using FluentAssertions;
+  using Sitecore.Security.Authentication;
+  using Xunit;
+
+  public class AuthenticationManagerTest
+  {
+    [Fact]
+    public void ShouldLogin()
+    {
+      // arrange
+      AuthenticationManager.Login("admin").Should().BeTrue();
+    }
+  }
+}
