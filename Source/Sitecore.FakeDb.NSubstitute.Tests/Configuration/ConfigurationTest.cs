da5c636b322b512cc7968b2d8cf83a472e472575
Sergey Shushlyapin <sshushlyapin@gmail.com>
2014-04-02 01:05:11 +0200

    Introduced NSubstitute configuration factory

---
+++ Source/Sitecore.FakeDb.NSubstitute.Tests/Configuration/ConfigurationTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.FakeDb.NSubstitute.Tests.Configuration
+{
+  using FluentAssertions;
+  using Sitecore.Configuration;
+  using Sitecore.Security.AccessControl;
+  using Xunit;
+
+  public class ConfigurationTest
+  {
+    [Fact]
+    public void ShouldResolveNSubstituteFactory()
+    {
+      // act & assert
+      Factory.CreateObject("factories/factory", true).Should().BeOfType<NSubstituteFactory>();
+    }
+
+    [Fact]
+    public void ShouldCreateAuthorizationProviderMock()
+    {
+      // act & assert
+      AuthorizationManager.Provider.Should().BeAssignableTo<AuthorizationProvider>();
+    }
+  }
+}
---
+++ Source/Sitecore.FakeDb.NSubstitute.Tests/NSubstituteFactoryTest.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.FakeDb.NSubstitute.Tests
+{
+  using System.Configuration.Provider;
+  using FluentAssertions;
+  using Sitecore.Security.AccessControl;
+  using Xunit;
+
+  public class NSubstituteFactoryTest
+  {
+    private readonly NSubstituteFactory factory;
+
+    public NSubstituteFactoryTest()
+    {
+      this.factory = new NSubstituteFactory();
+    }
+
+    [Fact]
+    public void ShouldCreateSubstituteForType()
+    {
+      // arrange
+      var type = "Sitecore.Security.AccessControl.AuthorizationProvider, Sitecore.Kernel";
+
+      // act
+      var obj = this.factory.GetObject(type);
+
+      // assert
+      obj.Should().BeAssignableTo<AuthorizationProvider>();
+    }
+
+    [Fact]
+    public void ShouldSetProviderNameIfTypeIsProvider()
+    {
+      // arrange
+      var providerType = "System.Configuration.Provider.ProviderBase, System.Configuration";
+
+      // act
+      var providerMock = (ProviderBase)this.factory.GetObject(providerType);
+
+      // assert
+      providerMock.Name.Should().Be("ProviderBase");
+    }
+  }
+}
