5bf80b0ed9b4cfc02d5b2eb41c9317e2372488a0
Sergey Shushlyapin <sshushlyapin@gmail.com>
2014-04-06 09:39:28 +0200

    Implemented config transformation

--- Source/Sitecore.FakeDb.NSubstitute/Properties/AssemblyInfo.cs
+++ Source/Sitecore.FakeDb.NSubstitute/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.FakeDb.NSubstitute")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A unit testing framework for Sitecore.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sergey Shushlyapin")]
 [assembly: AssemblyProduct("Sitecore.FakeDb.NSubstitute")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
+[assembly: AssemblyInformationalVersion("0.9.1")]
--- Source/Sitecore.FakeDb/Properties/AssemblyInfo.cs
+++ Source/Sitecore.FakeDb/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.FakeDb")]
-[assembly: AssemblyDescription("A friendly unit testing framework for Sitecore.")]
+[assembly: AssemblyDescription("A unit testing framework for Sitecore.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sergey Shushlyapin")]
 [assembly: AssemblyProduct("Sitecore.FakeDb")]
