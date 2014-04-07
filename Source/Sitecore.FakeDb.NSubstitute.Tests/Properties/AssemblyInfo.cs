13c0531913d68a07e081546c8f121fc1837ace31
Sergey Shushlyapin <ss@sitecore.net>
2014-04-07 14:24:44 +0200

    Introduced common assembly version info. Do not transform main app.config

--- Source/Sitecore.FakeDb.NSubstitute.Tests/Properties/AssemblyInfo.cs
+++ Source/Sitecore.FakeDb.NSubstitute.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -21,17 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4cd6eddc-8187-4e66-85f6-80347cb9bcb4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
-[assembly: AssemblyInformationalVersion("0.9.0")]
---
+++ Source/Sitecore.FakeDb/Properties/AssemblyVersionInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyInformationalVersion("0.10.0")]
