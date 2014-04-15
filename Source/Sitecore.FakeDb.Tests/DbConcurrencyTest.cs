c9d0b43225ab9d9f54df65a5ed363658cc0b59bf
Sergey Shushlyapin <ss@sitecore.net>
2014-04-15 13:26:10 +0300

    Added concurrency test

---
+++ Source/Sitecore.FakeDb.Tests/DbConcurrencyTest.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.FakeDb.Tests
+{
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using FluentAssertions;
+  using Xunit;
+
+  public class DbConcurrencyTest
+  {
+    [Fact(Skip = "To be solved")]
+    public void ShouldBeThreadSafe()
+    {
+      // arrange
+      var create = new Task(Create);
+      var assert = new Task(Assert);
+
+      create.Start();
+      assert.Start();
+
+      create.Wait();
+      assert.Wait();
+    }
+
+    private static void Create()
+    {
+      var random = new Random();
+
+      using (var db = new Db { new DbItem("Home") })
+      {
+        Thread.Sleep(random.Next(1000));
+        db.GetItem("/sitecore/content/home").Should().NotBeNull("the Home item is expected");
+      }
+    }
+
+    private static void Assert()
+    {
+      var random = new Random();
+
+      using (var db = new Db())
+      {
+        Thread.Sleep(random.Next(1000));
+        db.GetItem("/sitecore/content/home").Should().BeNull("the Home item is not expected");
+      }
+    }
+  }
+}
