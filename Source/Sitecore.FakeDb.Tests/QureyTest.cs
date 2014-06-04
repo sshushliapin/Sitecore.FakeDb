3d809921643bd2f33861ced7836cbbf1243076f7
Pavel Veller <pveller@gmail.com>
2014-06-03 23:20:17 -0400

    Make sure *Root items are registered as children of the fake /sitecore item in order to support Query API

---
+++ Source/Sitecore.FakeDb.Tests/QureyTest.cs
@@ -0,0 +1,38 @@
+﻿namespace Sitecore.FakeDb.Tests
+{
+  using FluentAssertions;
+  using Sitecore.Data;
+  using Sitecore.Data.Items;
+  using Xunit.Extensions;
+
+  public class QureyTest
+  {
+
+    [Theory]
+    [InlineData("/sitecore/content/home")]
+    [InlineData("/sitecore/content/*")]
+    [InlineData("/sitecore/content/*[@@key = 'home']")]
+    public void ShouldSupportQuery(string query)
+    {
+      // arrange
+      var homeId = ID.NewID;
+
+      // ToDo (?): add setting (and cleaning) up Context.Database to the Db constructor/dispose
+      var ctxDb = Context.Database;
+
+      using (var db = new Db() { new DbItem("home", homeId) })
+      {
+        Context.Database = db.Database; // Query needs it
+
+        // act
+        Item[] result = Sitecore.Data.Query.Query.SelectItems(query);
+
+        Context.Database = ctxDb;
+
+        // assert 
+        result.Should().HaveCount(1);
+        result[0].ID.Should().Be(homeId);
+      }
+    }
+  }
+}
