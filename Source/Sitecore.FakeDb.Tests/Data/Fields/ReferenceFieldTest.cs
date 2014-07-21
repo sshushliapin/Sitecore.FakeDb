3675350c8e37a5bce51da1362756daf9e2255e27
sergeyshushlyapin <sshushlyapin@gmail.com>
2014-07-22 01:15:00 +0300

    Added ReferenceFieldTest

---
+++ Source/Sitecore.FakeDb.Tests/Data/Fields/ReferenceFieldTest.cs
@@ -0,0 +1,59 @@
+﻿namespace Sitecore.FakeDb.Tests.Data.Fields
+{
+  using FluentAssertions;
+  using Sitecore.Data;
+  using Sitecore.Data.Fields;
+  using Xunit;
+
+  public class ReferenceFieldTest
+  {
+    [Fact]
+    public void ShouldGetTargetByPath()
+    {
+      // arrange
+      var targetId = ID.NewID;
+
+      using (var db = new Db
+                        { 
+                           new DbItem("source") { { "Reference", "/sitecore/content/target" } },
+                           new DbItem("target",  targetId)
+                        })
+      {
+        var item = db.GetItem("/sitecore/content/source");
+
+        // act
+        var referenceField = (ReferenceField)item.Fields["Reference"];
+
+        // assert
+        referenceField.Database.Should().Be(db.Database);
+        referenceField.Path.Should().Be("/sitecore/content/target");
+        referenceField.TargetID.Should().Be(targetId);
+        referenceField.TargetItem.Should().Be(db.GetItem(targetId));
+      }
+    }
+    [Fact]
+    public void ShouldGetTargetById()
+    {
+      // arrange
+      var targetId = ID.NewID;
+
+      using (var db = new Db
+                        { 
+                           new DbItem("source") { { "Reference", targetId.ToString() } },
+                           new DbItem("target",  targetId)
+                        })
+      {
+        var item = db.GetItem("/sitecore/content/source");
+
+        // act
+        var referenceField = (ReferenceField)item.Fields["Reference"];
+
+        // assert
+        referenceField.Database.Should().Be(db.Database);
+        referenceField.Path.Should().Be(targetId.ToString());
+        referenceField.TargetID.Should().Be(targetId);
+        referenceField.TargetItem.Should().Be(db.GetItem(targetId));
+      }
+    }
+  }
+}
