c28da02bc151988a8786570d7a5d2b61c58dbd05
sergeyshushlyapin <sshushlyapin@gmail.com>
2014-08-14 12:02:33 +0300

    Added concurrency tests for pipelines and settings

--- Source/Sitecore.FakeDb.Tests/DbConcurrencyTest.cs
+++ Source/Sitecore.FakeDb.Tests/DbConcurrencyTest.cs
@@ -5,43 +5,89 @@
   using System.Threading.Tasks;
   using FluentAssertions;
   using Xunit;
+  using Sitecore.Xml;
+  using Sitecore.Configuration;
+  using Sitecore.Pipelines;
 
   public class DbConcurrencyTest
   {
     [Fact]
-    public void ShouldBeThreadSafe()
+    public void ShouldBeThreadLocalDb()
     {
       // arrange
-      var create = new Task(Create);
-      var assert = new Task(Assert);
+      var t1 = Task.Factory.StartNew(() =>
+        {
+          var random = new Random();
+          using (var db = new Db { new DbItem("Home") })
+          {
+            Thread.Sleep(random.Next(1000));
+            db.GetItem("/sitecore/content/home").Should().NotBeNull("the Home item is expected");
+          }
+        });
 
-      create.Start();
-      assert.Start();
+      var t2 = Task.Factory.StartNew(() =>
+        {
+          var random = new Random();
+          using (var db = new Db())
+          {
+            Thread.Sleep(random.Next(1000));
+            db.GetItem("/sitecore/content/home").Should().BeNull("the Home item is not expected");
+          }
+        });
 
-      create.Wait();
-      assert.Wait();
+      t1.Wait();
+      t2.Wait();
     }
 
-    private static void Create()
+    [Fact(Skip = "To be implemented.")]
+    public void ShouldBeThreadLocalPipelines()
     {
-      var random = new Random();
-
-      using (var db = new Db { new DbItem("Home") })
+      var t1 = Task.Factory.StartNew(() =>
       {
-        Thread.Sleep(random.Next(1000));
-        db.GetItem("/sitecore/content/home").Should().NotBeNull("the Home item is expected");
-      }
+        using (var db = new Db())
+        {
+          db.PipelineWatcher.Expects("mypipeline");
+
+          Thread.Sleep(1000);
+
+          CorePipeline.Run("mypipeline", new PipelineArgs());
+          db.PipelineWatcher.EnsureExpectations();
+        }
+      });
+
+      var t2 = Task.Factory.StartNew(() =>
+      {
+        using (var db = new Db()) { }
+      });
+
+      t1.Wait();
     }
 
-    private static void Assert()
+    [Fact(Skip = "To be implemented.")]
+    public void ShouldBeThreadLocalSettings()
     {
-      var random = new Random();
-
-      using (var db = new Db())
+      var t1 = Task.Factory.StartNew(() =>
       {
-        Thread.Sleep(random.Next(1000));
-        db.GetItem("/sitecore/content/home").Should().BeNull("the Home item is not expected");
-      }
+        using (var db = new Db())
+        {
+          db.Configuration.Settings["mysetting"] = "123";
+
+          Thread.Sleep(1000);
+
+          Settings.GetSetting("mysetting").Should().Be("123");
+        }
+      });
+
+      var t2 = Task.Factory.StartNew(() =>
+      {
+        using (var db = new Db())
+        {
+          db.Configuration.Settings["mysetting"] = "abc";
+          Settings.GetSetting("mysetting").Should().Be("abc");
+        }
+      });
+
+      t1.Wait();
     }
   }
 }
