580578a5572c8bb9f36c7236ed65f1d0f393456a
sergeyshushlyapin <sshushlyapin@gmail.com>
2014-09-13 01:14:04 +0300

    Added list of fast query expressions based on the FQ guide (http://sdn.sitecore.net/upload/sdn5/developer/using%20sitecore%20fast%20query/using%20sitecore%20fast%20query001.pdf)

--- Source/Sitecore.FakeDb.Tests/Data/Query/FastQueryTest.cs
+++ Source/Sitecore.FakeDb.Tests/Data/Query/FastQueryTest.cs
@@ -1,8 +1,11 @@
 ﻿namespace Sitecore.FakeDb.Tests.Data.Query
 {
+  using System;
+  using System.Linq;
   using FluentAssertions;
   using Sitecore.Data;
   using Xunit;
+  using Xunit.Extensions;
 
   public class FastQueryTest
   {
@@ -72,33 +75,76 @@
       }
     }
 
-    [Fact]
-    public void ShouldSelectItemByFieldName()
+    [Theory]
+    [InlineData("fast:/sitecore/content/Home/Products", "Products")]
+    [InlineData("fast:/sitecore/content/Home/Products/*", "Documents,Hammer,Jacket,Stylish Bag,Table")]
+    [InlineData("fast:/sitecore/content/Home//Circle", "Circle")]
+    [InlineData("fast:/sitecore/content/Home//Circle/..", "Shapes")]
+    [InlineData("fast:/sitecore/content/Home/*[@Title = 'Welcome to Sitecore']", "Greeting Page")]
+    [InlineData("fast:/sitecore/content/Home/Products/Hammer[@Available = '1']", "Hammer")]
+    [InlineData("fast://*[@@id = '{787EE6C5-0885-495D-855E-1D129C643E55}']", "Stylish Bag")]
+    [InlineData("fast:/sitecore/content/Home/Products/*[@@name = 'Hammer']", "Hammer")]
+    [InlineData("fast:/sitecore/content/Home/Products/*[@@key = 'hammer']", "Hammer")]
+    [InlineData("fast:/sitecore/content/Home//*[@@templateid = '{F348C2A0-73B8-4AF4-BD9E-2C5901908369}']", "Square,Triangle,Circle")]
+    [InlineData("fast:/sitecore/content/Home//*[@@templatename = 'Shape']", "Square,Triangle,Circle")]
+    [InlineData("fast:/sitecore/content/Home//*[@@templatekey = 'product']", "Stylish Bag,New Document,Table,Jacket,Old Document,Hammer")]
+
+    // TODO: Implement search by master id
+    // [InlineData("fast:/sitecore/content/Home//*[@@masterid='{47D22B69-AADC-4A79-9347-E0AD225F84FB}']", "")]
+
+    // TODO: Implement search by parent id
+    // [InlineData("fast:/sitecore/content/Home//*[@@parentid = '{8F906BEE-BA50-429F-9DCC-3EC4794FA182}']", "Stylish Bag,Table,Jacket,Documents,Hammer")]
+    [InlineData("fast:/sitecore/content/Home//*[@@name='Jacket']/ancestor::*", "sitecore,Products,content,Home")]
+    [InlineData("fast:/sitecore/content/Home/Products/Documents/parent::*/child::*", "Documents,Hammer,Jacket,Stylish Bag,Table")]
+    [InlineData("fast:/sitecore/content/Home/Products/Documents/../*", "Documents,Hammer,Jacket,Stylish Bag,Table")]
+    [InlineData("fast:/sitecore/content/Home/*[@@name='Shapes']/*", "Circle,Square,Triangle")]
+    [InlineData("fast:/sitecore/content/Home/Shapes/*", "Circle,Square,Triangle")]
+    [InlineData("fast:/sitecore/content/Home/descendant::*[@@name='Documents' or @@name = 'Shapes']", "Shapes,Documents")]
+    [InlineData("fast:/sitecore/content/Home//*[@@name='Documents' or @@name = 'Shapes']", "Shapes,Documents")]
+    public void ShouldReturnItemsByQuery(string query, string items)
     {
       // arrange
-      using (var db = new Db { new DbItem("Home") { { "Title", "Welcome to Sitecore" } } })
+      var productTemplateId = ID.NewID;
+      var shapeTemplateId = new ID("{F348C2A0-73B8-4AF4-BD9E-2C5901908369}");
+
+      var expected = items.Split(new[] { "," }, StringSplitOptions.None);
+
+      using (var db = new Db
+                        {
+                          new DbTemplate("Product", productTemplateId) { "Available" },
+                          new DbTemplate("Shape", shapeTemplateId),
+
+                          new DbItem("Home")
+                            {
+                              new DbItem("Greeting Page") { { "Title", "Welcome to Sitecore" } },
+                              new DbItem("Products", new ID("{8F906BEE-BA50-429F-9DCC-3EC4794FA182}"))
+                                {
+                                  // TODO: Would be nice to guess the template id somehow
+                                  new DbItem("Hammer", ID.NewID, productTemplateId) { { "Available", "1" } },
+                                  new DbItem("Jacket", ID.NewID, productTemplateId),
+                                  new DbItem("Stylish Bag", new ID("{787EE6C5-0885-495D-855E-1D129C643E55}"), productTemplateId),
+                                  new DbItem("Table", ID.NewID, productTemplateId),
+                                  new DbItem("Documents", ID.NewID, ID.NewID)
+                                    {
+                                      new DbItem("New Document", ID.NewID, productTemplateId),
+                                      new DbItem("Old Document", ID.NewID, productTemplateId),
+                                    }
+                                },
+                              new DbItem("Shapes")
+                                {
+                                  new DbItem("Circle", ID.NewID, shapeTemplateId),
+                                  new DbItem("Square", ID.NewID, shapeTemplateId),
+                                  new DbItem("Triangle", ID.NewID, shapeTemplateId),
+                                },
+                              new DbItem("Query Test")
+                            }
+                        })
       {
         // act
-        var item = db.Database.SelectSingleItem("fast:/sitecore/content/*[@Title = 'Welcome to Sitecore']");
+        var actual = db.Database.SelectItems(query).Select(i => i.Name);
 
         // assert
-        item.Should().NotBeNull();
-        item.Name.Should().Be("Home");
-      }
-    }
-
-    [Fact]
-    public void ShouldSelectItemById()
-    {
-      // arrange
-      using (var db = new Db { new DbItem("Home", new ID("{787EE6C5-0885-495D-855E-1D129C643E55}")) })
-      {
-        // act
-        var item = db.Database.SelectSingleItem("fast://*[@@id = '{787EE6C5-0885-495D-855E-1D129C643E55}']");
-
-        // assert
-        item.Should().NotBeNull();
-        item.Name.Should().Be("Home");
+        actual.Should().BeEquivalentTo(expected);
       }
     }
   }
