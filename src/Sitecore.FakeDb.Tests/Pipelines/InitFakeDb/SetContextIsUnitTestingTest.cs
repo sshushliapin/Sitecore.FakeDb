9e3c08885b4000fb9018e5999ad9c82b0121b96e
sergeyshushlyapin <sshushlyapin@gmail.com>
2014-10-29 01:40:55 +0200

    Implemented feature request #26 from @mikeedwards83 (Include Configs)

---
+++ src/Sitecore.FakeDb.Tests/Pipelines/InitFakeDb/SetContextIsUnitTestingTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.FakeDb.Tests.Pipelines.InitFakeDb
+{
+  using FluentAssertions;
+  using Sitecore.FakeDb.Pipelines.InitFakeDb;
+  using Sitecore.Pipelines;
+  using Xunit;
+
+  public class SetContextIsUnitTestingTest
+  {
+    [Fact]
+    public void ShouldSetContextIsUnitTesting()
+    {
+      // arrange
+      var processor = new SetContextIsUnitTesting();
+
+      // act
+      processor.Process(new PipelineArgs());
+
+      // assert
+      Context.IsUnitTesting.Should().BeTrue();
+    }
+  }
+}
---
+++ src/Sitecore.FakeDb/Pipelines/InitFakeDb/SetContextIsUnitTesting.cs
@@ -0,0 +1,12 @@
+﻿namespace Sitecore.FakeDb.Pipelines.InitFakeDb
+{
+  using Sitecore.Pipelines;
+
+  public class SetContextIsUnitTesting
+  {
+    public virtual void Process(PipelineArgs args)
+    {
+      Context.IsUnitTesting = true;
+    }
+  }
+}
