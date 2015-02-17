600db569e27b8442e426a41bc81907bbbd0f353c
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-02-18 01:06:19 +0200

    Introduce FieldNamingHelper

---
+++ src/Sitecore.FakeDb/FieldNamingHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.FakeDb
+{
+  using System.Collections.Generic;
+  using Sitecore.Data;
+
+  public class FieldNamingHelper
+  {
+    public KeyValuePair<ID, string> GetFieldIdNamePair(ID id, string name)
+    {
+      return new KeyValuePair<ID, string>(id, name);
+    }
+  }
+}
