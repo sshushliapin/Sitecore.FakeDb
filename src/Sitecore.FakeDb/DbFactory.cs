61f112504c184512af3f8f543af1cb3d56020a71
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-02-20 01:26:45 +0200

    Create FakeDb Factory

---
+++ src/Sitecore.FakeDb/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.FakeDb
+{
+  using System;
+  using Sitecore.Data;
+  using Sitecore.Reflection;
+
+  public class DbFactory : IFactory
+  {
+    private static readonly ID id = ID.NewID;
+
+    public object GetObject(string identifier)
+    {
+      Console.Out.WriteLine("id = {0}", id);
+
+      var type = ReflectionUtil.GetTypeInfo(identifier);
+      var obj = Activator.CreateInstance(type);
+
+
+
+      return obj;
+    }
+  }
+}
