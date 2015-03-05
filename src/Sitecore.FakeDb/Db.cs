b92bd9ae1b52fc4ed81e585d3e0b45029d82ebf2
Sergey Shushlyapin <ss@sitecore.net>
2015-03-05 12:33:32 +0200

    Move 'noisy' database commands initialization out of the App.config file

--- src/Sitecore.FakeDb/Db.cs
+++ src/Sitecore.FakeDb/Db.cs
@@ -6,15 +6,18 @@
   using System.Xml;
   using Sitecore.Configuration;
   using Sitecore.Data;
+  using Sitecore.Data.Events;
   using Sitecore.Data.Items;
   using Sitecore.Diagnostics;
   using Sitecore.FakeDb.Configuration;
   using Sitecore.FakeDb.Data.Engines;
+  using Sitecore.FakeDb.Data.Engines.DataCommands;
   using Sitecore.FakeDb.Pipelines;
   using Sitecore.FakeDb.Pipelines.InitFakeDb;
   using Sitecore.FakeDb.Pipelines.ReleaseFakeDb;
   using Sitecore.Globalization;
   using Sitecore.Pipelines;
+  using Version = Sitecore.Data.Version;
 
   public class Db : IDisposable, IEnumerable
   {
@@ -33,6 +36,11 @@
     private XmlDocument config;
 
     private bool disposed;
+
+    static Db()
+    {
+      Database.InstanceCreated += DatabaseInstanceCreated;
+    }
 
     public Db()
       : this("master")
@@ -136,7 +144,7 @@
     {
       Assert.ArgumentNotNullOrEmpty(language, "language");
 
-      return this.Database.GetItem(id, Language.Parse(language), Sitecore.Data.Version.Parse(version));
+      return this.Database.GetItem(id, Language.Parse(language), Version.Parse(version));
     }
 
     public Item GetItem(string path)
@@ -159,7 +167,7 @@
       Assert.ArgumentNotNullOrEmpty(path, "path");
       Assert.ArgumentNotNullOrEmpty(language, "language");
 
-      return this.Database.GetItem(path, Language.Parse(language), Sitecore.Data.Version.Parse(version));
+      return this.Database.GetItem(path, Language.Parse(language), Version.Parse(version));
     }
 
     /// <summary>
@@ -206,6 +214,31 @@
       this.config = Factory.GetConfiguration();
 
       return this.config;
+    }
+
+    private static void DatabaseInstanceCreated(object sender, InstanceCreatedEventArgs e)
+    {
+      var commands = e.Database.Engines.DataEngine.Commands;
+
+      commands.AddFromTemplatePrototype = new AddFromTemplateCommand();
+      commands.AddVersionPrototype = new AddVersionCommand();
+      commands.BlobStreamExistsPrototype = new BlobStreamExistsCommand();
+      commands.CopyItemPrototype = new CopyItemCommand();
+      commands.CreateItemPrototype = new CreateItemCommand();
+      commands.DeletePrototype = new DeleteItemCommand();
+      commands.GetBlobStreamPrototype = new GetBlobStreamCommand();
+      commands.GetChildrenPrototype = new GetChildrenCommand();
+      commands.GetItemPrototype = new GetItemCommand();
+      commands.GetParentPrototype = new GetParentCommand();
+      commands.GetRootItemPrototype = new GetRootItemCommand();
+      commands.GetVersionsPrototype = new GetVersionsCommand();
+      commands.HasChildrenPrototype = new HasChildrenCommand();
+      commands.MoveItemPrototype = new MoveItemCommand();
+      commands.RemoveDataPrototype = new RemoveDataCommand();
+      commands.RemoveVersionPrototype = new RemoveVersionCommand();
+      commands.ResolvePathPrototype = new ResolvePathCommand();
+      commands.SaveItemPrototype = new SaveItemCommand();
+      commands.SetBlobStreamPrototype = new SetBlobStreamCommand();
     }
   }
 }
