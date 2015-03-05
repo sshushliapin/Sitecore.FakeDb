476763b64ebe9a642ee3630cf968e9d891327c6b
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-03-05 21:43:24 +0200

    Register config section handler as an appropriate entry point for subscribing to the Database.InstanceCreated global event

--- src/Sitecore.FakeDb/Db.cs
+++ src/Sitecore.FakeDb/Db.cs
@@ -37,11 +37,6 @@
 
     private bool disposed;
 
-    static Db()
-    {
-      Database.InstanceCreated += DatabaseInstanceCreated;
-    }
-
     public Db()
       : this("master")
     {
@@ -214,31 +209,6 @@
       this.config = Factory.GetConfiguration();
 
       return this.config;
-    }
-
-    private static void DatabaseInstanceCreated(object sender, InstanceCreatedEventArgs e)
-    {
-      var commands = e.Database.Engines.DataEngine.Commands;
-
-      commands.AddFromTemplatePrototype = new AddFromTemplateCommand();
-      commands.AddVersionPrototype = new AddVersionCommand();
-      commands.BlobStreamExistsPrototype = new BlobStreamExistsCommand();
-      commands.CopyItemPrototype = new CopyItemCommand();
-      commands.CreateItemPrototype = new CreateItemCommand();
-      commands.DeletePrototype = new DeleteItemCommand();
-      commands.GetBlobStreamPrototype = new GetBlobStreamCommand();
-      commands.GetChildrenPrototype = new GetChildrenCommand();
-      commands.GetItemPrototype = new GetItemCommand();
-      commands.GetParentPrototype = new GetParentCommand();
-      commands.GetRootItemPrototype = new GetRootItemCommand();
-      commands.GetVersionsPrototype = new GetVersionsCommand();
-      commands.HasChildrenPrototype = new HasChildrenCommand();
-      commands.MoveItemPrototype = new MoveItemCommand();
-      commands.RemoveDataPrototype = new RemoveDataCommand();
-      commands.RemoveVersionPrototype = new RemoveVersionCommand();
-      commands.ResolvePathPrototype = new ResolvePathCommand();
-      commands.SaveItemPrototype = new SaveItemCommand();
-      commands.SetBlobStreamPrototype = new SetBlobStreamCommand();
     }
   }
 }
