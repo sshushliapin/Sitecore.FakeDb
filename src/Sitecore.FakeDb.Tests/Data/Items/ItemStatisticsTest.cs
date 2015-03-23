6343aaa04146d8ccc1ac3d05195142b3e44bf377
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-03-24 00:57:01 +0200

    Add UpdateStatistics test

---
+++ src/Sitecore.FakeDb.Tests/Data/Items/ItemStatisticsTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.FakeDb.Tests.Data.Items
+{
+  using FluentAssertions;
+  using Sitecore.Data.Items;
+  using Sitecore.Security.Accounts;
+  using Sitecore.SecurityModel;
+  using Xunit;
+
+  public class ItemStatisticsTest
+  {
+    [Fact]
+    public void ShouldUpdateStatistics()
+    {
+      // arrange
+      using (var db = new Db { new DbItem("Home") })
+      {
+        var item = db.GetItem("/sitecore/content/home");
+
+        // act
+        using (new UserSwitcher("Editor", false))
+        using (new EditContext(item, SecurityCheck.Disable))
+        {
+          item.Statistics.UpdateRevision();
+        }
+
+        // assert
+        item.Statistics.UpdatedBy.Should().Be("Editor");
+      }
+    }
+  }
+}
