04587fc14a44a36ac7d13b28044e327bc2e75e6b
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-05-07 02:10:13 +0300

    Register default field types (#46)

---
+++ src/Sitecore.FakeDb.Tests/Data/Fields/FieldTypeManagerTest.cs
@@ -0,0 +1,32 @@
+﻿namespace Sitecore.FakeDb.Tests.Data.Fields
+{
+  using FluentAssertions;
+  using Sitecore.Data.Fields;
+  using Xunit;
+
+  public class FieldTypeManagerTest
+  {
+    [Fact]
+    public void ShouldGetField()
+    {
+      // arrange
+      using (var db = new Db
+                        {
+                          new DbItem("home")
+                            {
+                              new DbField("Is Active") { Type = "Checkbox", Value = "1" }
+                            }
+                        })
+      {
+        var home = db.GetItem("/sitecore/content/home");
+
+        // act
+        var customField = FieldTypeManager.GetField(home.Fields["Is Active"]);
+
+        // assert
+        customField.Should().NotBeNull();
+        customField.Value.Should().Be("1");
+      }
+    }
+  }
+}
