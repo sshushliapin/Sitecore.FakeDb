ca2ad851cd03462d755a9a9348a70ec17b7d0305
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-05-14 01:02:47 +0300

    Check can get all the fields (except Layout and Tracking) #46

--- src/Sitecore.FakeDb.Tests/Data/Fields/FieldTypeManagerTest.cs
+++ src/Sitecore.FakeDb.Tests/Data/Fields/FieldTypeManagerTest.cs
@@ -6,26 +6,69 @@
 
   public class FieldTypeManagerTest
   {
-    [Fact]
-    public void ShouldGetField()
+    // TODO: Implement Layout and Tracking fields.
+    [Theory]
+    [InlineData("Checkbox")]
+    [InlineData("Date")]
+    [InlineData("Datetime")]
+    [InlineData("File")]
+    [InlineData("Image")]
+    [InlineData("Rich Text")]
+    [InlineData("Single-Line Text")]
+    [InlineData("Word Document")]
+    [InlineData("Multi-Line Text")]
+    [InlineData("Checklist")]
+    [InlineData("Droplist")]
+    [InlineData("Grouped Droplink")]
+    [InlineData("Grouped Droplist")]
+    [InlineData("Multilist")]
+    [InlineData("Multilist with Search")]
+    [InlineData("Name Value List")]
+    [InlineData("Treelist")]
+    [InlineData("Treelist with Search")]
+    [InlineData("TreelistEx")]
+    [InlineData("Droplink")]
+    [InlineData("Droptree")]
+    [InlineData("General Link")]
+    [InlineData("General Link with Search")]
+    [InlineData("Version Link")]
+    [InlineData("Frame")]
+    [InlineData("Rules")]
+
+    // [InlineData("Tracking")]
+    [InlineData("Datasource")]
+    [InlineData("Custom")]
+    [InlineData("Internal Link")]
+
+    // [InlineData("Layout")]
+    [InlineData("Template Field Source")]
+    [InlineData("File Drop Area")]
+    [InlineData("Page Preview")]
+    [InlineData("Rendering Datasource")]
+    [InlineData("Thumbnail")]
+    [InlineData("Security")]
+    [InlineData("UserList")]
+    [InlineData("html")]
+    [InlineData("link")]
+    [InlineData("lookup")]
+    [InlineData("reference")]
+    [InlineData("text")]
+    [InlineData("memo")]
+    [InlineData("tree")]
+    [InlineData("tree list")]
+    [InlineData("valuelookup")]
+    public void ShouldGetField(string fieldType)
     {
       // arrange
       using (var db = new Db
                         {
-                          new DbItem("home")
-                            {
-                              new DbField("Is Active") { Type = "Checkbox", Value = "1" }
-                            }
+                          new DbItem("home") { new DbField("field") { Type = fieldType } }
                         })
       {
         var home = db.GetItem("/sitecore/content/home");
 
-        // act
-        var customField = FieldTypeManager.GetField(home.Fields["Is Active"]);
-
-        // assert
-        customField.Should().NotBeNull();
-        customField.Value.Should().Be("1");
+        // act & assert
+        FieldTypeManager.GetField(home.Fields["field"]).Should().NotBeNull();
       }
     }
   }
