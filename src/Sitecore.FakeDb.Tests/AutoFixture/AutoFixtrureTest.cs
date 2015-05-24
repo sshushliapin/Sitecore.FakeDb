7a3830a9a27d636e253a757af06de32d7a4f058b
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-05-25 01:16:06 +0300

    Implement the db.GetEnumerator method (#61)
    Ensure can instantiate a db instance using AutoFixture's AutoData attribute

---
+++ src/Sitecore.FakeDb.Tests/AutoFixture/AutoFixtrureTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.FakeDb.Tests.AutoFixture
+{
+  using FluentAssertions;
+  using Ploeh.AutoFixture.Xunit2;
+  using Xunit;
+
+  public class AutoFixtrureTest
+  {
+    [Theory, AutoData]
+    public void ShouldCreateDb(Db db)
+    {
+      // arrange
+      db.Add(new DbItem("home"));
+
+      // act & assert
+      db.GetItem("/sitecore/content/home").Should().NotBeNull();
+    }
+  }
+}
