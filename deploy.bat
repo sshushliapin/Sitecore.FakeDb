33bc98d3c6a6ce9719d4a22afc73dabb1ea8fc46
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-06-05 00:03:48 +0300

    Remove unused msbuild properties

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ setlocal
 :PROMPT
 SET /P AREYOUSURE=Are you sure you want to publish the NuGet packages (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-  C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Build.msbuild /property:Configuration=Release /target:Deploy
+  C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe Build.msbuild /target:Deploy
 :END
 endlocal
