94c0ecf6a7f48462484cd354d3ac2d91e92ee274
Sergey Shushlyapin <ss@sitecore.net>
2015-07-03 15:22:11 +0300

    Add AutoDbCustomization samples

---
+++ src/Sitecore.FakeDb.AutoFixture.Tests/Samples.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.FakeDb.AutoFixture.Tests
+{
+  using FluentAssertions;
+  using Ploeh.AutoFixture;
+  using Ploeh.AutoFixture.Xunit2;
+  using Sitecore.Data.Items;
+  using Sitecore.Pipelines;
+  using Sitecore.Rules;
+  using Xunit;
+
+  public class Samples
+  {
+    [Theory, AutoDbData]
+    public void ShouldCreateItemInstance(Item item)
+    {
+      item.Should().NotBeNull();
+    }
+
+    [Theory, AutoDbData]
+    public void ShouldCreatePipelineArgs(PipelineArgs args)
+    {
+      args.Should().NotBeNull();
+    }
+
+    [Theory, AutoDbData]
+    public void ShouldCreateRuleContext(RuleContext context)
+    {
+      context.Should().NotBeNull();
+    }
+  }
+
+  internal class AutoDbDataAttribute : AutoDataAttribute
+  {
+    public AutoDbDataAttribute()
+      : base(new Fixture().Customize(new AutoDbCustomization()))
+    {
+    }
+  }
+}
