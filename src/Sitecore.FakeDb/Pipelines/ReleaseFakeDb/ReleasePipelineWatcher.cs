ae436f535e67fe227fe074575a5ca6efe2d3dbce
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-07-06 20:16:08 +0300

    Move the CorePipelineFactory.CleanCache() call to the PipelineWatcher.Dispose() method. Fix unit tests clean up

--- src/Sitecore.FakeDb/Pipelines/ReleaseFakeDb/ReleasePipelineWatcher.cs
+++ src/Sitecore.FakeDb/Pipelines/ReleaseFakeDb/ReleasePipelineWatcher.cs
@@ -1,13 +1,10 @@
 ﻿namespace Sitecore.FakeDb.Pipelines.ReleaseFakeDb
 {
-  using Sitecore.Pipelines;
-
   public class ReleasePipelineWatcher
   {
     public virtual void Process(ReleaseDbArgs args)
     {
       args.Db.PipelineWatcher.Dispose();
-      CorePipelineFactory.ClearCache();
     }
   }
 }
