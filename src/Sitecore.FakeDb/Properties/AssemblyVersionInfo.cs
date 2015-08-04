0914f05be3d6aa4a4752c8c6c8ef343edde8af9e
Sergey Shushlyapin <ss@sitecore.net>
2015-08-04 19:00:06 +0300

    Update version info

--- src/Sitecore.FakeDb/Properties/AssemblyVersionInfo.cs
+++ src/Sitecore.FakeDb/Properties/AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.28.1.0")]
-[assembly: AssemblyFileVersion("0.28.1.0")]
-[assembly: AssemblyInformationalVersion("0.28.1")]
+[assembly: AssemblyVersion("0.29.0.0")]
+[assembly: AssemblyFileVersion("0.29.0.0")]
+[assembly: AssemblyInformationalVersion("0.29.0")]
