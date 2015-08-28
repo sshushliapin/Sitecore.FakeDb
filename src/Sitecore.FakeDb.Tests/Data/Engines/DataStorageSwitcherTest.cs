98a4b74b07385d195efccb7a64143efd81e84768
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-08-29 00:08:33 +0300

    Create DataStorageSwitcher (#78)

---
+++ src/Sitecore.FakeDb.Tests/Data/Engines/DataStorageSwitcherTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Sitecore.FakeDb.Tests.Data.Engines
+{
+  using Ploeh.AutoFixture.Xunit2;
+  using Sitecore.Common;
+  using Sitecore.FakeDb.Data.Engines;
+  using Xunit;
+
+  public class DataStorageSwitcherTest
+  {
+    [Theory, DefaultAutoData]
+    public void ShouldBeSwitcher(DataStorageSwitcher sut)
+    {
+      Assert.IsAssignableFrom<Switcher<DataStorage>>(sut);
+    }
+
+    [Theory, DefaultAutoData]
+    public void ShouldSwitchDataStorage([Frozen]DataStorage dataStorage, DataStorageSwitcher sut)
+    {
+      Assert.Same(dataStorage, Switcher<DataStorage>.CurrentValue);
+    }
+  }
+}
