7385f00cef61ca8612cf9a2e9ebfe3d42f239ce6
sergeyshushlyapin <sshushlyapin@gmail.com>
2015-10-14 15:47:04 +0300

    Introduce DatabaseNameGenerator

---
+++ src/Sitecore.FakeDb.AutoFixture/DatabaseNameGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace Sitecore.FakeDb.AutoFixture
+{
+  using System;
+  using Ploeh.AutoFixture.Kernel;
+
+  public class DatabaseNameGenerator : ISpecimenBuilder
+  {
+    public object Create(object request, ISpecimenContext context)
+    {
+      if (request == null)
+      {
+        return new NoSpecimen();
+      }
+
+      var seededRequest = request as SeededRequest;
+      if (seededRequest == null)
+      {
+        return new NoSpecimen();
+      }
+
+      if (typeof(string) != seededRequest.Request as Type)
+      {
+        return new NoSpecimen();
+      }
+
+      var seed = (string)seededRequest.Seed;
+      if (seed != "databaseName" && seed != "DatabaseName")
+      {
+        return new NoSpecimen();
+      }
+
+      return "master";
+    }
+  }
+}
---
+++ test/Sitecore.FakeDb.AutoFixture.Tests/DatabaseNameGeneratorTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Sitecore.FakeDb.AutoFixture.Tests
+{
+  using FluentAssertions;
+  using Ploeh.AutoFixture.Kernel;
+  using Xunit;
+
+  public class DatabaseNameGeneratorTest
+  {
+    [Fact]
+    public void SutIsISpecimenBuilder()
+    {
+      Assert.IsAssignableFrom<ISpecimenBuilder>(new DatabaseNameGenerator());
+    }
+
+    [Fact]
+    public void CreateReturnsNoSpecimenIfRequestIsNull()
+    {
+      var sut = new DatabaseNameGenerator();
+      sut.Create(null, null).Should().BeOfType<NoSpecimen>();
+    }
+
+    [Fact]
+    public void CreateReturnsNoSpecimenIfRequestIsNotSeededRequest()
+    {
+      var sut = new DatabaseNameGenerator();
+      sut.Create(new object(), null).Should().BeOfType<NoSpecimen>();
+    }
+
+    [Fact]
+    public void CreateReturnsNoSpecimenIfRequestSeedTypeIsNotString()
+    {
+      var sut = new DatabaseNameGenerator();
+      sut.Create(new SeededRequest(typeof(object), null), null).Should().BeOfType<NoSpecimen>();
+    }
+
+    [Fact]
+    public void CreateReturnsNoSpecimenIfRequestSeedNameIsNotDatabaseName()
+    {
+      var sut = new DatabaseNameGenerator();
+      sut.Create(new SeededRequest(typeof(string), "some name"), null).Should().BeOfType<NoSpecimen>();
+    }
+
+    [Theory]
+    [InlineData("databaseName")]
+    [InlineData("DatabaseName")]
+    public void CreateReturnsMasterForSeededStringRequest(string seed)
+    {
+      var sut = new DatabaseNameGenerator();
+      sut.Create(new SeededRequest(typeof(string), seed), null).Should().Be("master");
+    }
+  }
+}
