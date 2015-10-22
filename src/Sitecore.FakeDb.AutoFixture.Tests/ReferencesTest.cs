e00b15665b4af5c70ff74a661158172f1707be01
Sergey Shushlyapin <ss@sitecore.net>
2015-10-22 18:01:24 +0300

    #96: Referencing latest versions of AutoFixture causes version conflicts when updating Nuget packages
    Downgrade to the minimal acceptable AutoFixture version

---
+++ src/Sitecore.FakeDb.AutoFixture.Tests/ReferencesTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.FakeDb.AutoFixture.Tests
+{
+  using System;
+  using System.Linq;
+  using FluentAssertions;
+  using Xunit;
+
+  public class ReferencesTest
+  {
+    /// <summary>
+    /// https://github.com/sergeyshushlyapin/Sitecore.FakeDb/issues/96
+    /// </summary>
+    [Fact]
+    public void ShouldReferenceSpecificAutoFixtureVersion()
+    {
+      typeof(AutoDbCustomization).Assembly.GetReferencedAssemblies()
+        .First(x => x.Name == "Ploeh.AutoFixture")
+        .Version.Should().Be(new Version(3, 30, 0, 0));
+    }
+  }
+}
