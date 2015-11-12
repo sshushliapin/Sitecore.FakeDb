349cf49fda69832d9b2c587d53cc9382b6c3f6ac
Pavel Veller <pveller@gmail.com>
2015-11-12 18:54:11 -0500

    Fix #93 via ToString() in ItemWrapper. Posted a wish/issue to FluentAssertions as well

---
+++ src/Sitecore.FakeDb.Tests/Runtime/FluentAssertionsTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Sitecore.FakeDb.Tests.Runtime
+{
+  using System;
+  using FluentAssertions;
+  using Xunit;
+
+  public class FluentAssertionsTest
+  {
+    [Fact]
+    public void ShouldNotTimeOutOnFailingFluentAssertion()
+    {
+      // arrange
+      using (var db = new Db
+      {
+        new DbItem("home")
+      })
+      {
+        var home = db.GetItem("/sitecore/content/home");
+
+        Action assertion = () =>
+        {
+          try { home.Should().BeNull(); } catch { }
+        };
+
+        // act && assert
+        assertion.ExecutionTime().ShouldNotExceed(1.Seconds());
+      }
+    }
+  }
+}
