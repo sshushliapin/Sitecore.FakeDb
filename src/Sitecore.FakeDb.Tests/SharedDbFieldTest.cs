c2cc913eaa27279c50f3cdebd799209bb4f02805
Sergey Shushlyapin <sshushlyapin@gmail.com>
2015-12-23 18:14:21 +0200

    Move SharedDbField into separate file

--- src/Sitecore.FakeDb.Tests/SharedDbFieldTest.cs
+++ src/Sitecore.FakeDb.Tests/SharedDbFieldTest.cs
@@ -63,38 +63,4 @@
       sut.GetValue("da", 1).Should().Be(expected);
     }
   }
-
-  public class SharedDbField : DbField
-  {
-    public SharedDbField(ID id)
-      : base(id)
-    {
-      this.Shared = true;
-    }
-
-    public SharedDbField(string name)
-      : base(name)
-    {
-      this.Shared = true;
-    }
-
-    public SharedDbField(string name, ID id)
-      : base(name, id)
-    {
-      this.Shared = true;
-    }
-
-    public override void Add(string language, int version, string value)
-    {
-      base.Add(language, version, value);
-
-      foreach (var langValue in this.Values)
-      {
-        for (var i = langValue.Value.Count - 1; i > 0; --i)
-        {
-          langValue.Value[i] = value;
-        }
-      }
-    }
-  }
 }
