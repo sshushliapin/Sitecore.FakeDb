0dabee3e6ec65f28f5ef6d821869c0033aa36845
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-03-12 23:15:39 +0200

    Reorganize solution structure

---
+++ test/Sitecore.FakeDb.Serialization.Tests/SerializationId.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.FakeDb.Serialization.Tests
+{
+  using Sitecore.Data;
+
+  public static class SerializationId
+  {
+    public static readonly ID ContentHomeItem = new ID("{110D559F-DEA5-42EA-9C1C-8A5DF7E70EF9}");
+
+    public static readonly ID SampleItemTemplate = new ID("{76036F5E-CBCE-46D1-AF0A-4143F9B557AA}");
+
+    public static readonly ID SampleTemplateFolder = new ID("{73BAECEB-744D-4D4A-A7A5-7A935638643F}");
+  }
+}
