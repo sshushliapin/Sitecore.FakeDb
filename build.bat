8b13ff422d56764d43698c26c1586dd6954bfccd
Sergey Shushlyapin <sshushlyapin@gmail.com>
2016-03-14 10:01:26 +0200

    Disable missing documentation warnings

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions
+%WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions /p:NoWarn=1591
