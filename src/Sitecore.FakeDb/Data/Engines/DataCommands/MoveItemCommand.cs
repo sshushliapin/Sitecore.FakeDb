82cc9a837025c74eae2e11cd6a491165306dbc32
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-04-22 00:59:52 +0300

    Implement the 'MoveItem' data provider method

--- src/Sitecore.FakeDb/Data/Engines/DataCommands/MoveItemCommand.cs
+++ src/Sitecore.FakeDb/Data/Engines/DataCommands/MoveItemCommand.cs
@@ -3,6 +3,7 @@
   using System;
   using Sitecore.Diagnostics;
 
+  [Obsolete("The commands are not expected to be used anymore. All the logic moved to the DataProvider.")]
   public class MoveItemCommand : Sitecore.Data.Engines.DataCommands.MoveItemCommand
   {
     private readonly DataStorage dataStorage;
