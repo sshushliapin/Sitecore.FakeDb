a314217fe11eb8d56b98e092672326999c85e922
sergeyshushlyapin <sshushlyapin@gmail.com>
2016-06-19 10:44:44 +0300

    Create Sitecore.FakeDb.NUnitLite.Tests project

---
+++ test/Sitecore.FakeDb.NUnitLite.Tests/LicenseWatcherShutdownTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.FakeDb.NUnitLite.Tests
+{
+  using NUnit.Framework;
+  using Sitecore.FakeDb;
+
+  /// <summary>
+  /// Test for issue https://github.com/sergeyshushlyapin/Sitecore.FakeDb/issues/133.
+  /// </summary>
+  public class LicenseWatcherShutdownTest
+  {
+    [Test]
+    public void ShouldShutdownLicenseWatcher()
+    {
+      using (var db = new Db())
+      {
+        db.GetItem("/sitecore/content");
+      }
+    }
+  }
+}
