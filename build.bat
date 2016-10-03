87d2206e07dad5ee8e6e557f3e687ec8402d1b40
Sergey Shushlyapin <sshushlyapin@gmail.com>
2016-10-04 00:39:17 +0300

    Enable building with Sitecore 8.2 and .NET 4.5.2

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-rem %WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions /p:NoWarn=1591 /p:TargetFrameworkVersion=v4.5.2 /p:SupportedSitecoreVersions=SC820 /p:PatchDatabase=true
-if %errorlevel% neq 0 exit /b %errorlevel%
 %WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions /p:NoWarn=1591
