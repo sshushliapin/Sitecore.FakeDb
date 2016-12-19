ceaf0a55b55ad3ee9224598e5d478428a100b05e
Sergey Shushlyapin <sshushlyapin@gmail.com>
2016-12-19 22:26:22 +0200

    Set revisions for all languages specified for particular item

--- src/Sitecore.FakeDb/Pipelines/AddDbItem/SetStatistics.cs
+++ src/Sitecore.FakeDb/Pipelines/AddDbItem/SetStatistics.cs
@@ -1,5 +1,6 @@
 ﻿namespace Sitecore.FakeDb.Pipelines.AddDbItem
 {
+  using System.Linq;
   using Sitecore.Data;
 
   public class SetStatistics
@@ -13,9 +14,25 @@
 
       item.Fields.Add(new DbField("__Created", FieldIDs.Created) { Value = date });
       item.Fields.Add(new DbField("__Created by", FieldIDs.CreatedBy) { Value = user });
-      item.Fields.Add(new DbField("__Revision", FieldIDs.Revision) { Value = ID.NewID.ToString() });
+      item.Fields.Add(new DbField("__Revision", FieldIDs.Revision));
       item.Fields.Add(new DbField("__Updated", FieldIDs.Updated) { Value = date });
       item.Fields.Add(new DbField("__Updated by", FieldIDs.UpdatedBy) { Value = user });
+
+      SetRevisionForAllLanguages(item);
+    }
+
+    private static void SetRevisionForAllLanguages(DbItem item)
+    {
+      foreach (var lang in item.Fields.SelectMany(field => field.Values))
+      {
+        var revisionField = item.Fields[FieldIDs.Revision];
+        var revisionSet = !string.IsNullOrEmpty(
+          revisionField.GetValue(lang.Key, Version.Latest.Number));
+        if (!revisionSet)
+        {
+          revisionField.SetValue(lang.Key, ID.NewID.ToString());
+        }
+      }
     }
   }
 }
---
+++ test/Sitecore.FakeDb.Tests/Pipelines/SetStatisticsTest.cs
@@ -0,0 +1,52 @@
+﻿namespace Sitecore.FakeDb.Tests.Pipelines
+{
+  using FluentAssertions;
+  using Ploeh.AutoFixture.Xunit2;
+  using Sitecore.Data;
+  using Sitecore.FakeDb.Pipelines.AddDbItem;
+  using Xunit;
+
+  public class SetStatisticsTest
+  {
+    [Theory, DefaultAutoData]
+    public void ProcessSetsUniqueRevisionsForAllLanguages(
+      SetStatistics sut,
+      [Frozen] DbItem item,
+      AddDbItemArgs args,
+      ID id1,
+      ID id2,
+      string value1,
+      string value2)
+    {
+      var fieldEn = new DbField(id1) { { "en", value1 } };
+      var fieldDa = new DbField(id2) { { "da", value2 } };
+      item.Fields.Add(fieldEn);
+      item.Fields.Add(fieldDa);
+
+      sut.Process(args);
+      var revisionEn = item.Fields[FieldIDs.Revision].GetValue("en", 0);
+      var revisionDa = item.Fields[FieldIDs.Revision].GetValue("da", 0);
+
+      revisionEn.Should().NotBeNullOrEmpty();
+      revisionDa.Should().NotBeNullOrEmpty();
+      revisionEn.Should().NotBe(revisionDa);
+    }
+
+    [Theory, DefaultAutoData]
+    public void ProcessSetsValidRevision(
+      SetStatistics sut,
+      [Frozen] DbItem item,
+      AddDbItemArgs args,
+      ID id,
+      string value)
+    {
+      var field = new DbField(id) { { "en", value } };
+      item.Fields.Add(field);
+
+      sut.Process(args);
+      var actual = item.Fields[FieldIDs.Revision].GetValue("en", 0);
+
+      Assert.True(ID.IsID(actual));
+    }
+  }
+}
