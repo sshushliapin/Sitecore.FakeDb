ba7c2275c7edcae4ea148f7c1dc253a9a8c94097
Sergey Shushlyapin <sshushlyapin@gmail.com>
2016-12-21 17:20:53 +0200

    Update version info

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
-﻿1.3.4
+﻿1.3.5
+* [FIX] #158: Unable to substitute IDTable.GetKeys("prefix") by prefix method
+
+1.3.4
 * [FIX] #156:Generate item statistics for all the specified languages (#155)
 
 1.3.3
--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.4.0")]
-[assembly: AssemblyFileVersion("1.3.4.0")]
-[assembly: AssemblyInformationalVersion("1.3.4")]
+[assembly: AssemblyVersion("1.3.5.0")]
+[assembly: AssemblyFileVersion("1.3.5.0")]
+[assembly: AssemblyInformationalVersion("1.3.5")]
