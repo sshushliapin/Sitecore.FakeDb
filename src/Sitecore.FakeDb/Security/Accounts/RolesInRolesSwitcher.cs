a6c42d3e423d5da2782ac8a6800f69daa3ecdcbc
Sergey Shushlyapin <sshushlyapin@gmail.com>
2017-03-25 21:18:46 +0200

    Make FakeDb provider switchers obsolete
    since the providers became obsolete in Sitecore 8.2
    and will be removed in the next major release (#175).

--- src/Sitecore.FakeDb/Security/Accounts/RolesInRolesSwitcher.cs
+++ src/Sitecore.FakeDb/Security/Accounts/RolesInRolesSwitcher.cs
@@ -1,7 +1,14 @@
 ﻿namespace Sitecore.FakeDb.Security.Accounts
 {
+  using System;
   using Sitecore.Security.Accounts;
 
+  [Obsolete("Starting from Sitecore 8.2, the " +
+            "Sitecore.Security.Accounts.RolesInRolesProvider " +
+            "class is marked as obsolete and will be removed " +
+            "in the next major release. Please use new abstract " +
+            "type Sitecore.Abstractions.BaseRolesInRolesManager " +
+            "from the Sitecore.Kernel assembly.")]
   public class RolesInRolesSwitcher : ThreadLocalProviderSwitcher<RolesInRolesProvider>
   {
     public RolesInRolesSwitcher(RolesInRolesProvider localProvider)
