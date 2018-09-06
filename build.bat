b46295c4546e5bae35bedd6a2e246acf607d80f1
Serhii Shushliapin <sshushlyapin@gmail.com>
2018-09-07 02:08:12 +0300

    Add msbuild logger

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions /p:NoWarn=1591
+%WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Versions /p:NoWarn=1591 /fileLogger
