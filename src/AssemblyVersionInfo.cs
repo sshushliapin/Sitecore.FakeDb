4c7f18f9cb47b591336ac2d14c4a1ec7c35a1c5a
Serhii Shushliapin <sshushlyapin@gmail.com>
2018-10-24 22:53:02 +0300

    Update version

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0-alpha1")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha1")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta1")]
