725f729210bbd4ce782f57e0fb05c0437658c458
Serhii Shushliapin <sshushlyapin@gmail.com>
2018-10-26 17:17:05 +0300

    Log msbuild output

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ setlocal
 :PROMPT
 SET /P AREYOUSURE=Are you sure you want to publish the NuGet packages (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-  %WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Deploy /p:NoWarn=1591
+  %WINDIR%/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe Build.msbuild /p:Configuration=Release /t:Deploy /p:NoWarn=1591 /filelogger
 :END
 endlocal
